f2254e4ba6a878b8e7f5868d49c7cfa8ec5ccb3e
alami <alami@km.ru>
2021-03-20 15:31:15 +0500

    ch12:p170 Creating the preview component

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,27 +1,10 @@
 import React from "react"
 import Layout from "../components/Layout"
 import MenuCategory from "../components/MenuCategory"
-import { graphql, useStaticQuery } from "gatsby"
+import { graphql } from "gatsby"
 import styles from "./menu.module.css"
 
-export default function Menu() {
-  const data = useStaticQuery(graphql`
-  {
-    markdownRemark(frontmatter: { contentKey: { eq: "menu" } }) {
-      frontmatter {
-        title
-        categories {
-          name
-          items {
-            name
-            description
-            price
-          }
-        }
-      }
-    }
-  }
-`);
+export default function Menu({ data }) {
   return (
     <Layout>
       <div id={styles.main}>
@@ -37,3 +20,21 @@ export default function Menu() {
     </Layout>
   );
 }
+
+export const query = graphql`
+    {
+      markdownRemark(frontmatter: { contentKey: { eq: "menu" } }) {
+        frontmatter {
+          title
+          categories {
+            name
+            items {
+              name
+              description
+              price
+            }
+          }
+        }
+      }
+    }
+  `;
